0f5c077ff016195b61c422a614bff52314ae4ea4
verafadeeva <lunatik.yar@gmail.com>
2023-10-23 14:00:51 +0300

    Add selectors for auxilary endpoints

--- apps/api/v1/serializers/about.py
+++ apps/api/v1/serializers/about.py
@@ -3,7 +3,14 @@ from rest_framework import serializers
 
 class InfoOutputSerializer(serializers.Serializer):
     """
-    Сериализатор для моделей City, Profession
+    Сериализатор для модели City
     """
     id = serializers.IntegerField(read_only=True)
     title = serializers.CharField()
+
+
+class DirectionOfSudySerialiser(InfoOutputSerializer):
+    """
+    Сериализатор для DirectionOf Study
+    """
+    professions = InfoOutputSerializer(many=True, read_only=True)
--- apps/api/v1/views/about.py
+++ apps/api/v1/views/about.py
@@ -1,24 +1,33 @@
 from rest_framework import generics
 from rest_framework.permissions import AllowAny
 
-# from apps.about.selectors import get_cities, get_professions
-from apps.api.v1.serializers.about import InfoOutputSerializer
+from apps.about.selectors import get_cities, get_direction_of_study
+from apps.api.v1.serializers.about import (
+    InfoOutputSerializer,
+    DirectionOfSudySerialiser,
+)
 
 
-# TODO: сделать селекторы для получения кверисетов (сортировать по алфавиту)
 class CityView(generics.ListAPIView):
+    """
+    View для отдачи списка городов
+    """
     serializer_class = InfoOutputSerializer
     permission_classes = (AllowAny,)
 
     def get_queryset(self):
-        # queryset = get_cities()
-        return super().get_queryset()
+        queryset = get_cities()
+        return queryset
 
 
 class ProfessionView(generics.ListAPIView):
-    serializer_class = InfoOutputSerializer
+    """
+    View для отдачи списка направлений обучения и профессий, относящихся
+    к этим направлениям
+    """
+    serializer_class = DirectionOfSudySerialiser
     permission_classes = (AllowAny,)
 
     def get_queryset(self):
-        # queryset = get_professions()
-        return super().get_queryset()
+        queryset = get_direction_of_study()
+        return queryset
