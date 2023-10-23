31769cd3d3f0f59146a9440a8e238e4cba305631
verafadeeva <lunatik.yar@gmail.com>
2023-10-23 22:59:56 +0300

    Add adminmodels for about

--- apps/about/admin.py
+++ apps/about/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
 
-# Register your models here.
+from apps.about.models import City, Profession, DirectionOfStudy
+
+
+@admin.register(City)
+class CityAdmin(admin.ModelAdmin):
+    list_display = ("id", "title")
+    empty_value_display = "-пусто-"
+
+
+@admin.register(Profession)
+class ProfessionAdmin(admin.ModelAdmin):
+    list_display = ("id", "title", "direction")
+    empty_value_display = "-пусто-"
+
+
+@admin.register(DirectionOfStudy)
+class DirectionOfStudyAdmin(admin.ModelAdmin):
+    list_display = ("id", "title")
+    empty_value_display = "-пусто-"
